aaddd4ce8f7aa15cb7bfd40d4fde9037b5fa115a
Muhammad Miram <139574757+miraameditz@users.noreply.github.com>
2024-09-13 22:58:20 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,25 @@
+// console.log("Hello World");
+
+// for (var test = 0; test < 10; test++) {
+//     console.log("Running Loops")
+// }
+
+// for (var table = 1; table <= 10; table++) {
+//     console.log("2" + " x " + table + " = " + table * 2)
+// }
+
+var output = document.getElementById("output")
+var input = document.getElementById("Table_print")
+var tableError = document.getElementById("tableError")
+
+function prinTable() {
+    tableError.innerHTML = "";
+    if (input.value >= 1) {
+        for (var table = 1; table <= 10; table++) {
+            // console.log("2" + " x " + table + " = " + table * 2)   ///console testing
+            output.innerHTML += `${input.value} x  ${table} =  ${table * +input.value} <br />`
+        }
+    } else {
+        tableError.innerHTML = "Warning: Please enter a number to generate its multiplication table. Without a valid input, the table cannot be displayed."
+    }
+}
